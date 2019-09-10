b6cf39a722d992437b712e2a9a836ea8b801a728
dg <dim.glynatsis@gmail.com>
2019-09-10 20:07:38 +0300

    - add tests for update and getAll in todo service

--- test/todo-service.js
+++ test/todo-service.js
@@ -4,12 +4,18 @@ const mongoose = require('mongoose');
 const expect = require('chai').expect;
 const config = require('../config');
 
-describe('Todo Controller', function() {
+describe('Todo Service', function() {
     before(async function(){
         await mongoose.connect(`${config.MONGOURI}?retryWrites=true`, { useNewUrlParser: true });
     });
 
-    it('should add the todo in the database', async function(){ 
+    it('should return an array of the todos', async function() {
+        const todos = await todoService.getAll();
+        
+        expect(todos).to.be.an('array');
+    });
+
+    it('should add a new todo in the database', async function(){ 
         const text = 'This is a test todo';
         const tags = ['tag1', 'tag2', 'tag3'];
         const payload = {
@@ -24,7 +30,26 @@ describe('Todo Controller', function() {
         expect(todo.text).to.equals(text);
         expect(todo.dueDate).to.equal(null);
     });
-    
+
+    it('should update a todo and return the new object', async function() {
+        const todos = await todoService.getAll();
+
+        const todoId = todos[0]._id;
+        const newData = {
+            text: 'This is some fancy todo',
+            tags: ['newtag'],
+            dueDate: '2019-07-15T00:00:00.000Z'
+        }
+
+        const updatedTodo = await todoService.update(todoId, newData);
+
+        expect(updatedTodo.text).to.equal(newData.text);
+        updatedTodo.tags.forEach((tag, index) => {
+            expect(tag).to.equal(newData.tags[index]);
+        });
+        expect(updatedTodo.dueDate.toString()).to.equal((new Date(newData.dueDate)).toString());
+    })
+
     after(async function(){
         await Todo.deleteMany({});
         await mongoose.disconnect();
