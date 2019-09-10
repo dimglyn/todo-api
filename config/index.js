fa9c14ea3a22f6390ded6a90ba69142c13576d77
dg <dim.glynatsis@gmail.com>
2019-09-10 22:35:57 +0300

    - add configuration for mongo atlas

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 module.exports = {
-    MONGOURI: `mongodb://${process.env.HOST ? 'mongo' : 'localhost:27017'}/todoApp`,
+    MONGOURI: process.env.NODE_ENV === 'stage' ? 
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-fux9z.mongodb.net/todoApp?retryWrites=true&w=majority` 
+        : `mongodb://${process.env.NODE_ENV === 'development' ? 'mongo' : 'localhost:27017'}/todoApp`,
     HOST: process.env.HOST || 'localhost',
     PORT: process.env.PORT || 8080   
 }
