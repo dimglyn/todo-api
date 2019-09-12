91f7413e187318fc6314708482cc56bb32a27d67
dg <dim.glynatsis@gmail.com>
2019-09-13 01:13:22 +0300

    add dockerrun for aws eb

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@ module.exports = {
         `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-fux9z.mongodb.net/todoApp?retryWrites=true&w=majority` 
         : `mongodb://${process.env.NODE_ENV === 'development' ? 'mongo' : 'localhost:27017'}/todoApp`,
     HOST: process.env.HOST || 'localhost',
-    PORT: process.env.PORT || 8080   
+    PORT: process.env.PORT || 8080
 }
