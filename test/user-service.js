e9c793e2db2def7fcac06872e6fb4657e3f95d8b
Dimtiris <dim.glynatsis@gmail.com>
2020-03-09 23:00:06 +0200

    extend tests for user service

--- test/user-service.js
+++ test/user-service.js
@@ -9,25 +9,42 @@ import bcrypt from 'bcryptjs'
 import UserDAO from '../src/dao/user'
 const userService = new UserService({userDAO: new UserDAO()})
 
+const dummyUser = {
+  name: 'testuser',
+  email: 'test@test.com',
+  password: '123456'
+}
+
 describe('User Service', function() {
     before(async function(){
-        await mongoose.connect(`${config.MONGOURI}?retryWrites=true`, { useNewUrlParser: true, useFindAndModify: false })
+        await mongoose.connect(`${config.MONGOURI}Test?retryWrites=true`, { useNewUrlParser: true, useFindAndModify: false })
     })
 
     it('should sign up a new user', async function(){ 
-        const payload = {
-            name: 'testuser',
-            email: 'test@test.com',
-            password: '123456'
-        }
-        const user = await userService.create(payload)
+        const user = await userService.create(dummyUser)
 
         expect(user).to.have.property('email')
         expect(user).to.have.property('name')
         expect(user).to.have.property('password')
         expect(user).to.have.property('todos').with.lengthOf(0)
-        const isPasswordEqual = await bcrypt.compare(payload.password, user.password);
+        const isPasswordEqual = await bcrypt.compare(dummyUser.password, user.password);
         expect(isPasswordEqual).to.equals(true)
+    })
+
+    it('should get a user by email', async function() {
+      const user = await userService.getByEmail(dummyUser.email)
+
+      expect(user).to.have.property('email')
+      expect(user.email).to.equal(dummyUser.email)
+    })
+
+    it('should validate a users password', async function() {
+      const user = await userService.getByEmail(dummyUser.email)
+
+      const check = await userService.validPassword(dummyUser.password, user.password)
+      const falsecheck = await userService.validPassword(dummyUser.password + 'some more', user.password)
+      expect(check).to.be.true
+      expect(falsecheck).to.be.false
     })
 
     after(async function(){
